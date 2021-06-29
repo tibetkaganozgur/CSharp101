a3d2659c20c3e37f49cabf0af50d11c458f128d1
tibetkaganozgur <tktibetkagan@gmail.com>
2021-06-29 16:20:47 +0300

    Ödev 2

---
+++ Odev2_ConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Odev2_ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, how are you?");
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+        }
+    }
+}
